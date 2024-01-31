cf2bd50cbeaf0a69f1b17bd48ea7cd4f1b2e9450
AlexKasema <lexxmulwa@gmail.com>
2024-01-31 15:28:47 +0300

    A program that displays the clock.

---
+++ Digital_Clock/README.md
@@ -0,0 +1,5 @@
+# Digital Clock Program
+* A function that displays the clock.
+
+## Screenshot
+![app images]("./app_images/time.png")
---
+++ Digital_Clock/clock.js
@@ -0,0 +1,19 @@
+//? Digital Clock Program
+
+function myClock() {
+    const now = new Date();
+
+    let hours = now.getHours();
+    const meridiem = hours > 12 ? "PM" : "AM";
+    hours = hours % 12 || 12;
+    hours = hours.toString().padStart(2, 0);
+    const minutes = now.getMinutes().toString().padStart(2, 0);
+    const seconds = now.getSeconds().toString().padStart(2, 0);
+
+    const timeString = `${hours}:${minutes}:${seconds} ${meridiem}`;
+
+    document.getElementById("clock").textContent = timeString;
+}
+
+myClock();
+setInterval(myClock, 1000);
